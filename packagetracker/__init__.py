16e0eabb56df2598c22e0b0a039e8ab73e89670d
Fernando Chorney <gitlab@djsbx.com>
2016-02-19 16:04:15 -0600

    Added url and validate functions

--- packagetracker/__init__.py
+++ packagetracker/__init__.py
@@ -12,10 +12,19 @@ class Package(object):
             # 'USPS': USPSInterface()
         }
 
-    def track(self, tracking_number):
+    def get_interface(self, tracking_number):
         shipper = None
         for shipper_id, iface in self.interfaces.iteritems():
             if iface.identify(tracking_number):
                 shipper = shipper_id
                 break
-        return self.interfaces[shipper].track(tracking_number)
+        return self.interfaces[shipper]
+
+    def track(self, tracking_number):
+        return self.get_interface(tracking_number).track(tracking_number)
+
+    def url(self, tracking_number):
+        return self.get_interface(tracking_number).url(tracking_number)
+
+    def validate(self, tracking_number):
+        return self.get_interface(tracking_number).validate(tracking_number)
